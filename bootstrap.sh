ff32c597582bd0f1e52cfd1d87f3244219188448
Mg197119 <mg197119@outlook.com>
2025-04-21 15:12:31 -0400

    create venv

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,15 @@ echo "Updating packages…"
 apt-get update && apt-get upgrade -y
 
 
+
+echo "Creating Python venv..."
+python3 -m venv /root/my-bootstraps/venv
+
+
 echo "Installing Requirments..."
 cd /root/my-bootstraps || exit 1
-pip install -r requirments.txt
+/root/my-bootstraps/venv/bin/pip install --upgrade pip
+/root/my-bootstraps/venv/bin/pip install -r requirments.txt
 
 
 echo "Running Program..."
